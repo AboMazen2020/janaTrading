7b73d1b2574c4bb102048ee37d91aad10c885524
mohamed mamdouh <mohamedmmdouh950@gmail.com>
2021-09-30 13:44:55 +0300

    Update some files

--- js/master.js
+++ js/master.js
@@ -4,13 +4,8 @@ let span = document.querySelector(".up");
 
 window.onscroll = function () {
 
-    // if (this.scrollY >= 1000) {
-    //     span.classList.add("show");
-    // }else {
-    //     span.classList.remove("show");
-    // }
 
-    this.scrollY >= 700 ? span.classList.add("show") : span.classList.remove("show");
+    this.scrollY >= 500 ? span.classList.add("show") : span.classList.remove("show");
 }
 
 span.onclick = function () {
