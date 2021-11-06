0285ea25cae623a722343168027c5138f3528afd
mohamed mamdouh <mohamedmmdouh950@gmail.com>
2021-11-06 22:20:01 +0300

    Update 06-11-2021

--- js/master.js
+++ js/master.js
@@ -14,3 +14,29 @@ span.onclick = function () {
         behavior: "smooth",
     });
 };
+
+
+
+// let numb = document.querySelectorAll(".num");
+// let section = document.querySelector(".count-up");
+// let started = false;
+
+// window.onscroll = function () {
+//     if (window.scrollY >= section.offsetTop){
+//         if (!started) {
+//             numb.forEach((num) => statCount(num));    
+//         }
+//         started = true;
+//     }
+// }
+// function statCount(el) {
+//     let goal = el.dataset.goal;
+//     let count = setInterval(() => {
+//         el.textContent++;
+
+//         if (el.textContent == goal) {
+//             clearInterval(count);
+//         }
+//     }, 10);
+// }
+// statCount(document.querySelectorAll(".num")[0]);
